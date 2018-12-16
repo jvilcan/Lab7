5eaa503b04c5c5779a665957b3f38f1fbcfe6181
Javier <jvilcan@gmail.com>
2018-12-15 19:10:56 -0500

    Third Version

--- src/main/java/com/example/nh/lab7/MainActivity2.java
+++ src/main/java/com/example/nh/lab7/MainActivity2.java
@@ -1,37 +1,44 @@
 package com.example.nh.lab7;
 
-import android.content.Context;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.nh.lab7.utilities.NetworkUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     private EditText mSearchBoxEditText;
+
     private TextView mUrlDisplayTextView;
-    private TextView mSearchResultTextView;
+
+    private ListView mSearchResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
-        mSearchResultTextView = (TextView) findViewById(R.id.tv_github_search_result_json);
+        mSearchResultTextView = (ListView) findViewById(R.id.tv_github_search_result_json);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,10 +54,8 @@ public class MainActivity extends AppCompatActivity {
             //Toast.makeText(context, "opción de búsqueda", Toast.LENGTH_SHORT).show();
             makeGitHubSearchQuery();
         }
-
         return true;
     }
-
 
     private void makeGitHubSearchQuery() {
 
@@ -79,7 +84,6 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(URL... params) {
-
             URL searchUrl = params[0];
             String githubSearchResults = null;
 
@@ -96,22 +100,39 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String githubSearchResults) {
             if (githubSearchResults != null && !githubSearchResults.equals("")) {
-                mSearchResultTextView.setText(githubSearchResults);
+                //mSearchResultTextView.setText(githubSearchResults);
+                processJSON(githubSearchResults);
             }
-
-
         }
 
     }
 
+    private void processJSON(String githubSearchResults)
+    {
+        List<String> repos = new ArrayList<String>();
+        try
+        {
+            JSONObject json = new JSONObject(githubSearchResults);
+            JSONArray items = json.getJSONArray("items");
+            for(int i=0; i<items.length(); i++)
+            {
+                JSONObject object = items.getJSONObject(i);
+                repos.add(object.getString("html_url"));
+            }
 
+            // Llenar datos a lista por medio de adaptador.
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, repos);
+            if(mSearchResultTextView != null)
+            {
+                mSearchResultTextView.setAdapter(adapter);
 
-
-
-
-
-
-
-
+            }
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
 }
